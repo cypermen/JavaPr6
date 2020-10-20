e4a0bb7491db40912a1b943c51a2df22ff25e870
Andrey <andreybabich2012@yandex.ru>
2020-10-20 13:50:47 +0300

    Практика 6

---
+++ src/ru/mirea/Main.java
@@ -0,0 +1,8 @@
+package ru.mirea;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    TestClass.test();
+    }
+}
---
+++ src/ru/mirea/Student.java
@@ -0,0 +1,16 @@
+package ru.mirea;
+
+public class Student {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Student(int id) {
+    }
+}
